ae034219896fb58c12f110648ff6f52112282c06
Alex Bang <alexbang@hotmail.com>
2016-06-17 15:04:42 -0400

    Start JS function

--- js/script.js
+++ js/script.js
@@ -5,19 +5,20 @@
 
 $(document).ready(function(event){
 
-/*	$("body").on('click', 'add-button', function(event){
-		
+$('#add-button').on('click', function(event){
+	event.preventDefault();
+	addContact();
 	})
-	
-	$("body").on('click', '', function(event){
 
-	})
-*/
+	// $("body").on('click', '', function(event){
+
+	// })
+
 
 
 /*-------- VARIABLES --------*/
 	var contactInfo = {
-		firstName: 'john', 
+		firstName: 'john',
 		lastName: '',
 		phoneNumber: '',
 		street: '',
@@ -26,26 +27,46 @@ $(document).ready(function(event){
 	};
 
 	// Array to store contact objects, using a for loop to append First & Last name to <ul>
-	var contactList = [];	
-	
+	var contactList = [];
+
 
 
 /*---------- FUNCTIONS ----------*/
 
 // FUNCTION THAT CREATES NEW CONTACT OBJECT INHERITED FROM contactInfo OBJECT
 	function addContact() {
+		//create new contact by getting value of input field
+		for (var i = 0; i < contactList.length; i++){
+		}
 
+		var contact = Object.create(contactInfo);
+
+		contact.firstName = $('#first-name').val();
+		contact.lastName = $('#last-name').val();
+		contact.phoneNumber = $('#phone-number').val();
+		contact.street = $('#street').val();
+		contact.city = $('#city').val();
+		contact.city = $('#state').val();
+
+		contactList.push(contact);
+
+		for(var prop in contactList[0]) {
+		console.log(contact[prop]);
+	}
+
+
+		//assign unique name to each contact oject
 	}
 
 // FUNCTION THAT APPENDS NEW CONTACT OBJECT AS A LINK IN A LIST
 	function contactLink() {
 
 	}
-	
+
 // FUNCTION THAT DISPLAY CONTACT INFO IN show-contact DIV
 	function displayContact() {
 
 	}
-	
+
 
 });
