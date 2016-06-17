9b4473e8885b266c2606ea8263ca9c8f9cd8870b
Eric Snell <eric_snell21@yahoo.com>
2016-06-17 13:00:14 -0400

    function names

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 // CREATE A GENERAL CONTACT OBJECT
-
 // CREATE AN EVENT HANDLER THAT CREATES NEW CONTACT OBJECT
-
 // ALLOW USERS TO VIEW CONTACTS AS LINKS
-
 // WHEN LINK IS CLICKED, DISPLAY CONTACT INFO IN DIV
 
-
-
 $(document).ready(function(event){
+
+/*	$("body").on('click', 'add-button', function(event){
+		
+	})
+	
+	$("body").on('click', '', function(event){
+
+	})
+*/
+
 
 /*-------- VARIABLES --------*/
 	var contactInfo = {
@@ -22,26 +27,25 @@ $(document).ready(function(event){
 
 	// Array to store contact objects, using a for loop to append First & Last name to <ul>
 	var contactList = [];	
+	
 
 
 /*---------- FUNCTIONS ----------*/
 
 // FUNCTION THAT CREATES NEW CONTACT OBJECT INHERITED FROM contactInfo OBJECT
+	function addContact() {
 
-	$("body").on('click', 'add-button', function(event){
-		
-	})
+	}
 
 // FUNCTION THAT APPENDS NEW CONTACT OBJECT AS A LINK IN A LIST
+	function contactLink() {
 
-	$('.name-list').append()
-
+	}
+	
 // FUNCTION THAT DISPLAY CONTACT INFO IN show-contact DIV
+	function displayContact() {
 
-	$("body").on('click', '', function(event){
-
-	})
-
-
+	}
+	
 
 });
