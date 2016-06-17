a37c937791039893f4e03a54d9b10ac46b270724
Eric Snell <eric_snell21@yahoo.com>
2016-06-17 12:25:57 -0400

    basic html

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// CREATE A GENERAL CONTACT OBJECT
+
+// CREATE AN EVENT HANDLER THAT CREATES NEW CONTACT OBJECT
+
+// ALLOW USERS TO VIEW CONTACTS AS LINKS
+
+// WHEN LINK IS CLICKED, DISPLAY CONTACT INFO IN DIV
+
+
+
+$(document).ready(function(event){
+	var contactInfo = {
+		firstName: '', 
+		lastName: '',
+		phoneNumber: '',
+		street: '',
+		city: '',
+		state: ''
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
