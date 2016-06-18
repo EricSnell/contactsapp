d2a9a403e8bd4bfc94294f12e532ccb431a58712
Alex Bang <alexbang@hotmail.com>
2016-06-18 17:16:00 -0400

    fix bug continued

--- js/script.js
+++ js/script.js
@@ -34,12 +34,6 @@ $('#add-button').on('click', function(event){
 
 // FUNCTION THAT CREATES NEW CONTACT OBJECT INHERITED FROM contactInfo OBJECT
 	function addContact() {
-
-		// if ($('.#first-name').is(:empty)) {
-		// 	alert
-
-
-		}
 		//create new contact by getting value of input field
 		var contact = Object.create(contactInfo);
 		contact.firstName = $('#first-name').val();
@@ -55,7 +49,7 @@ $('#add-button').on('click', function(event){
 		for (var i = 0; i < contactList.length; i++) {
 			$('.name-list').append('<li data-index="' +i + '">' + contactList[i].firstName + ' ' + contactList[i].lastName + '</li>');
 		}
-	})
+	}
 
 // FUNCTION THAT CLEARS TEXT FIELD
 	function clearTextField() {
@@ -84,6 +78,7 @@ $('#add-button').on('click', function(event){
 //  		$(this).(contact.firstName)
 	})
 
+});
 
 
 
