1b4c22ac6dd70303b4628e6c09534b082f2b90d1
Alex Bang <alexbang@hotmail.com>
2016-06-18 21:10:07 -0400

    Add alternate number

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(event){
 
 // HIDE CONTACT DETAILS DIV (SHOW WHEN CONTACT NAME IS CLICKED)
 	$('.show-contact').hide();
+	$('.alternate').hide();
 
 // EVENT HANDLER THAT ADDS NEW CONTACT AND CLEARS TEXT INPUTS
 	$('#add-button').on('click', function(event){
@@ -13,6 +14,15 @@ $(document).ready(function(event){
 	});
 
 	displayContactInfo();
+
+// EVENT HANDLER THAT ADDS ANOTHER PHONE NUMBER
+	$('#add-phone').click(function() {
+		if ($('#first-name').val() === '' || $('#last-name').val() === '' || $('#phone-number').val() === '') {
+			alert('Please enter First Name, Last Name,  and Phone Number');
+		} else {
+		addPhoneNumb();
+		}
+	});
 
 /*----------- VARIABLES -----------*/
 
@@ -31,10 +41,11 @@ $(document).ready(function(event){
 
 /*------------- FUNCTIONS -------------*/
 
+var contact = Object.create(contactInfo);
+
 // FUNCTION THAT CREATES NEW CONTACT OBJECT INHERITED FROM contactInfo OBJECT
 	function addContact() {
 			//create new contact by getting value of input field
-			var contact = Object.create(contactInfo);
 // VALIDATE REQUIRED FORM FIELDS
 		if ($('#first-name').val() === '' || $('#last-name').val() === '' || $('#phone-number').val() === '') {
 			alert('Please enter First Name, Last Name,  and Phone Number');
@@ -49,6 +60,8 @@ $(document).ready(function(event){
 			// Push new object contact to the array containing all contacts
 			contactList.push(contact);
 
+			$('.alternate').hide();
+
 			// Clear the class of last contact and append new object with data attribute data-index
 			$('.name-list').empty();
 			for (var i = 0; i < contactList.length; i++) {
@@ -57,11 +70,20 @@ $(document).ready(function(event){
 		}
 	}
 
+
+//ADD SECOND PHONE NUMBER
+	function addPhoneNumb() {
+		contact.alternateNumber = $('#alternate-phone').val();
+		$('.alternate').show();
+		console.log('add second phone');
+	}
+
 // CLEARS TEXT FIELD
 	function clearTextField() {
 		$('#first-name').val('');
 		$('#last-name').val('');
 		$('#phone-number').val('');
+		$('#alternate-phone').val('');
 		$('#street').val('');
 		$('#city').val('');
 		$('#state').val('');
@@ -79,6 +101,7 @@ $(document).ready(function(event){
 			$('#detail-First').append('First Name:  ' + contact.firstName);
 			$('#detail-Last').append('Last Name:  ' + contact.lastName);
 			$('#detail-Phone').append('Phone Number:  ' + contact.phoneNumber);
+			$('#detail-Alternate-Phone').append('Alternate Number:  ' + contact.alternateNumber);
 			$('#detail-Address').append('Address: ');
 			$('#detail-Address-List').append('<li>' + contact.street + ' ' + contact.city + ' ' + contact.state + '</li>');
 			$('.show-contact').show();
