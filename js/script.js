1a8ef200ac6c906bc25148166d64350fcf5fe7c5
Alex Bang <alexbang@hotmail.com>
2016-06-19 13:04:10 -0400

    Adds email

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(event){
 // HIDE CONTACT DETAILS DIV (SHOW WHEN CONTACT NAME IS CLICKED)
 	$('.show-contact').hide();
 	$('.alternate').hide();
+	$('.email').hide();
 
 // EVENT HANDLER THAT ADDS NEW CONTACT AND CLEARS TEXT INPUTS
 	$('#add-button').on('click', function(event){
@@ -22,6 +23,11 @@ $(document).ready(function(event){
 			alert('Please enter First Name, Last Name,  and Phone Number');
 		} else {
 			$('.alternate').show();}
+	});
+
+// EVENT HANDLER THAT ADDS EMAIL
+$('#add-email').click(function() {
+		$('.email').show();
 	});
 
 /*----------- VARIABLES -----------*/
@@ -56,12 +62,14 @@ $(document).ready(function(event){
 			contact.city = $('#city').val();
 			contact.state= $('#state').val();
 			contact.alternateNumber = $('#alternate-phone').val();
+		 	contact.email = $('#email-address').val();
 		 	// console.log('add second phone');
 
 			// Push new object contact to the array containing all contacts
 			contactList.push(contact);
 
 			$('.alternate').hide();
+			$('.email').hide();
 
 			// Clear the class of last contact and append new object with data attribute data-index
 			$('.name-list').empty();
@@ -85,6 +93,7 @@ $(document).ready(function(event){
 		$('#last-name').val('');
 		$('#phone-number').val('');
 		$('#alternate-phone').val('');
+		$('#email-address').val('');
 		$('#street').val('');
 		$('#city').val('');
 		$('#state').val('');
@@ -104,6 +113,7 @@ $(document).ready(function(event){
 			$('#detail-Phone').append('Phone Number:  ' + contact.phoneNumber);
 			$('#detail-Address-List').append('<li>' + contact.street + ' ' + contact.city + ' ' + contact.state + '</li>');
 			$('#detail-Alternate-Phone').append('Alternate Number:  ' + contact.alternateNumber);
+			$('#detail-Email-Address').append('Email Address:  ' + contact.email);
 			$('#detail-Address').append('Address: ');
 			$('.show-contact').show();
 		});
