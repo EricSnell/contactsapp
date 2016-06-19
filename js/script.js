e53bfcea7796fe0b195ae3381b6d83fdedf05798
Eric Snell <eric_snell21@yahoo.com>
2016-06-19 14:06:04 -0400

    v2 start

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-//	if (!$('#first-name').val() === '' || !$('#phone-number').val() === '') 
-
 $(document).ready(function(event){
 
-// HIDE CONTACT DETAILS DIV (SHOW WHEN CONTACT NAME IS CLICKED)
-	$('.show-contact').hide(); 
+	// Hides contact details div
+	$('.show-contact').hide();
 
-// EVENT HANDLER THAT ADDS NEW CONTACT AND CLEARS TEXT INPUTS
+	// Adds contact 
 	$('#add-button').on('click', function(event){
 		event.preventDefault();	
 		addContact();			
@@ -14,10 +12,14 @@ $(document).ready(function(event){
 
 	displayContactInfo();
 
+	$('body').on('click','#add-number-button',function(event){	
+		event.preventDefault();
+		addPhoneNumber();
+	});
 
 /*----------- VARIABLES -----------*/
 
-	// OBJECT THAT ADDED CONTACTS WILL INHERIT FROM
+	// Object that contacts added will inherit from
 	var contactInfo = {
 		firstName: '',
 		lastName: '',
@@ -27,24 +29,21 @@ $(document).ready(function(event){
 		state: ''
 	};
 
-	// ARRAY TO STORE CONTACTS
+	// Array to store contact objects
 	var contactList = [];
 
 
 /*------------- FUNCTIONS -------------*/
 
-// FUNCTION THAT CHECKS TO MAKE SURE REQUIRED FIELDS IN FORM ARE FILLED
-
-
-
-// FUNCTION THAT CREATES NEW CONTACT OBJECT INHERITED FROM contactInfo OBJECT
+// Creates new contact object and pushes to array
 	function addContact() {
-			//create new contact by getting value of input field
-			var contact = Object.create(contactInfo);
+
+		var contact = Object.create(contactInfo);
 
 		if ($('#first-name').val() === '' || $('#phone-number').val() === '') {	
 			alert('Please enter First Name and Phone Number');
-		
+			return false;
+
 		} else {
 			contact.firstName = $('#first-name').val();
 			contact.lastName = $('#last-name').val();
@@ -53,10 +52,9 @@ $(document).ready(function(event){
 			contact.city = $('#city').val();
 			contact.state= $('#state').val();
 
-			// Push new object contact to the array containing all contacts
 			contactList.push(contact);
 
-			// Clear the class of last contact and append new object with data attribute data-index
+			// Empty previous list and display updated list
 			$('.name-list').empty();
 			for (var i = 0; i < contactList.length; i++) {
 				$('.name-list').append('<li data-index="' +i + '">' + contactList[i].firstName + ' ' + contactList[i].lastName + '</li>');
@@ -64,8 +62,17 @@ $(document).ready(function(event){
 		}
 	}
 
-// FUNCTION THAT CLEARS TEXT FIELD
+// Add new phone number input
+	function addPhoneNumber() {
+		('#phone-number').clone.append('#phone-number');
+	}
+
+// Add new street, city, and state input
+
+
+// Clears form after adding new contact
 	function clearTextField() {
+
 		$('#first-name').val('');
 		$('#last-name').val('');
 		$('#phone-number').val('');
@@ -74,8 +81,9 @@ $(document).ready(function(event){
 		$('#state').val('');
 	}
 
-// FUNCTION THAT DISPLAY CONTACT INFO IN show-contact DIV
+// Displays contact details when name is clicked
 	function displayContactInfo() {
+
 		$('.name-list').on('click', 'li', function(event){
 			var contactIndex = $(this).data("index")
 			var contact = contactList[parseInt(contactIndex)]
@@ -92,20 +100,4 @@ $(document).ready(function(event){
 			$('.show-contact').show();
 		});
 	}
-
-
-
-
-
 });
-
-
-
-
-
-
-
-
-
-
-
